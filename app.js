532e238e92e5ba12c9207769c8dde8b11f5cbd6a
Artem Koroliuk <artemkoroliuk86@gmail.com>
2022-01-25 21:35:10 +0200

    hw

--- app.js
+++ app.js
@@ -1,32 +1,53 @@
 /*jshint esversion: 6 */
-/*const ar20=[11,17,20,23,2];
-const arEven=ar20.filter((n, i, a) =>a.length%2==0 ? n %2==0 : n %2==1);
-console.log(arEven);
-const res=ar20.reduce((res,cur)=> res+cur,0);
-console.log(res);
-const max=ar20.reduce((max,cur)=>cur>max?cur :max,ar20[0])
-console.log(max);*/
-const person1={id:123,name:"moshe",address:{city:"lod",street:"sokolov"}};
-const person2={id:123,name:"moshe",address:{city:"lod",street:"sokolov"}};
-const person3=person1;
-console.log(`person1==person2 is ${person1==person2}`);
-console.log(`"123"==123 is ${"123"==123}`);
-console.log(`"123"===123 is ${"123"===123}`);
-console.log(`JSON.stringify(person1)===JSON.stringify(person2) is ${JSON.stringify(person1)===JSON.stringify(person2)}`);
-console.log(JSON.stringify(person1));
-console.log(`name is person1 is "${person1.name}"`);
-console.log(`live person1 in city "${person1.address.city}"`);
-Object.keys(person1).forEach(k=> console.log(k));
-Object.values(person1).forEach(k=> console.log(k));
-Object.entries(person1).forEach(k=> console.log(k));
-function createAddress(city,street){
-   // {city:city, street:street} <=> {city,street}
-    return {city:city, street:street}
+const arr=[2,5,8,4,9,4,16];
+function myForEach(array, callback) {
+        for (let i = 0; i < array.length; i++) {
+                callback(array[i], i, array);
+        }
 }
-function createPerson (id,name,address)
+myForEach(arr, n=>console.log(n));
 
+function myFiltr(array,callback){
+    const res=[];
 
+if(myForEach(array,callback.call(n,i,a))==true) {
 
+      res.push(array[i]);  
+}
+ 
+return res;
+}
+function big(max){return big>10;}
+const arr10=arr.myFilter(big);
+console.log(arr10);
 
+function myReduce(array,callback,initialResult){
+        let i=0;
+        if(arguments.length<2){
+                i=1;
+                result=array[0];
+        }
+function reduce1(n,i,a){
+        result=
+}
+/*
+function forEachCall(n, i, a) {
+        res.push(callback(n, i, a));
+}
+myForEach(array, forEachCall);
+return res;*/
 
-
+//////////////////////////////////////////
+function createAddress(city, street) {
+        //{city: city, street: street} <=> {city, street}
+        return {city, street}
+}
+function createPerson(id, name, address) {
+        return {id, name, address};
+}
+const persons = [
+        createPerson(123, "Vasya", createAddress("Rehovot","Parshani")),
+        createPerson(124, "Olya", createAddress("Rehovot","Pr. Plaut")),
+        createPerson(125, "Tolya", createAddress("Tel-Aviv","Dizengoff")),
+        createPerson(126, "Sara", createAddress('Lod', 'Sokolov'))
+]
