c3603848d090017a5d19378e73ab8362694b73cd
Artem Koroliuk <artemkoroliuk86@gmail.com>
2022-02-06 17:10:54 +0200

    HomeWokr17

--- app.js
+++ app.js
@@ -1,26 +1,91 @@
 /*jshint esversion: 6 */
-const circle={radius:20, square:function(){return 3.14*(this.radius)**2}
-, perimetr:()=>2*3.14*this.radius, toString: function(){
-        return `radius of this circle is ${this.radius}`
-}};
-console.log(`shape = ${circle.square()}, perimetr = ${circle.perimetr()}`);
-console.log(`circle ${circle}`);
-const circle1 ={radius:20
-, perimetr: function(){ 2*3.14*this.radius}, toString: function(){
-        return `radius of this circle is ${this.radius}`;
-}};
-function Circle(radius){
-        this.radius=radius;
+class Person{
+    #id
+    #name
+    constructor(id,name) {
+        this.#id=id
+        this.#name=name}
+    getId(){
+        return this.#id }
+    getName(){
+        return this.#name }
+    toString(){
+        return `id :${this.#id}  name:${this.#name}` }
 }
-Circle.prototype.square=function(){
-      return  3.14*(this.radius**2);
-}
-Circle.prototype.perimetr=function(){
-        return 2*3.14*this.radius;
-}
-Circle.prototype.toString=function(){
-        return `radius of this circle is ${this.radius}`;
-}
-const circle10=new Circle(10);
 
-console.log(circle10);
+class Child extends Person{
+    #kindergarden
+    constructor(id,name,kindergarden) {
+        super(id,name);
+        this.#kindergarden=kindergarden
+    }
+    getKinderGarden(){
+        return this.#kindergarden
+    }
+    toString() {
+        return super.toString() + `  kindergarden: ${this.#kindergarden}`;
+    }
+}
+class Employee extends Person{
+    #salary
+    constructor(id,name,salary) {
+        super(id,name);
+        this.#salary=salary
+    }
+    getSalary(){
+        return this.#salary
+    }
+    computeSalary(){
+return this.#salary
+    }
+    toString() {
+    return super.toString() + `  salary: ${this.computeSalary()}`;
+}
+}
+
+class wageEmployee extends Employee{
+    #hours
+    #wage
+    constructor(id,name, salary,hours,wage) {
+        super(id,name,salary);
+        this.#hours=hours
+        this.#wage=wage
+    }
+    getHours(){
+        return this.#hours
+    }
+    getWage(){
+        return this.#wage
+    }
+    computeSalary() {
+        return super.computeSalary()+ this.#hours*this.#wage;
+    }
+}
+const persons=[
+    new Child(100, 'Olya', 'Shalom'),
+    new Child(101, 'Sergey', 'Boker'),
+    new Child(102, 'Kolya', 'Shalom'),
+    new Employee(103, 'Vasya', 1000),
+    new wageEmployee(104, 'Tolya', 1000, 10, 100)
+]
+/////////////11111///////////////////////
+function  countOfPersonType(persons, type){
+    return resAray= persons.filter(el=>el.constructor.name===type).length
+}
+const typeWage=countOfPersonType(persons,'wageEmployee')
+console.log(typeWage)
+///////////////2///////////////
+    function computeSalaryBudget(persons){
+   // return persons.reduce((total,salary)=> total+=salary.computeSalary() ,0)
+       return persons.reduce((res, cur) => res += cur.computeSalary ? cur.computeSalary() : 0 , 0);
+    }
+const computeSBudget=computeSalaryBudget(persons)
+console.log(computeSBudget)
+
+/////////3/////////////////
+function countChildrenKindergarden(persons, kinderGardenName) {
+    return persons.filter(n => n.getKinderGarden && n.getKinderGarden() === kinderGardenName ).length
+
+}
+const garden=countChildrenKindergarden(persons,'Shalom')
+console.log(garden)
